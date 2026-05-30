41e95c5da05501c3bd227d18f973a55ca404eae1
SambucoS <salvatoresambuco@hotmail.com>
2026-05-30 09:32:08 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module emmepitre.com.emmepitre {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens emmepitre.com.emmepitre to javafx.fxml;
+    exports emmepitre.com.emmepitre;
+}
