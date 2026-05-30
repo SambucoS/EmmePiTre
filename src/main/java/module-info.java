9d9ce08a3240e29b56824c558b26bdb4c40883cd
Domenico <domenicoraimondo81@gmail.com>
2026-05-30 16:42:59 +0200

    delete delle prove e spostamento delle cartelle view da java/ a resources/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,30 +1,15 @@
-module EmmePiTre {
-    // Moduli base di JavaFX
+module emmepitre.com.emmepitre {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
     requires javafx.web;
-    requires javafx.media;
-    requires javafx.swing;
 
-    // Librerie esterne rilevate dal tuo build (Jackson per i file JSON)
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.annotation;
-
-    // Altre librerie nel tuo classpath (se le usi attivamente nel codice)
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
 
-    opens models to javafx.base, com.fasterxml.jackson.databind;
-    // Esportazioni e Aperture per il Launcher
+    opens emmepitre.com.emmepitre to javafx.fxml;
     exports emmepitre.com.emmepitre;
-    opens emmepitre.com.emmepitre to javafx.graphics, javafx.fxml;
-
-    // Apertura dei pacchetti per permettere a JavaFX di iniettare i nodi FXML
-    opens controllers to javafx.fxml;
-    // Se Jackson deve leggere/scrivere i tuoi modelli
 }
