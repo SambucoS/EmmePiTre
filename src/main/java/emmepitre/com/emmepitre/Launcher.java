0b046f896f57e1ec86eb0e2c63c66edaf5accb0a
SambucoS <salvatoresambuco@hotmail.com>
2026-05-30 10:22:06 +0200

    Cartelle

--- src/main/java/emmepitre/com/emmepitre/Launcher.java
+++
@@ -1,9 +0,0 @@
-package emmepitre.com.emmepitre;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
