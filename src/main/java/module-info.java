4e7ddaec2192ffc017caa230ae0b997e6844b8fe
Domenico <domenicoraimondo81@gmail.com>
2026-05-31 10:32:50 +0200

    Creazione della modale per la gestione dell'aggiunta traccia fxml e controller Aggiunta metodo equals e attributo album alla classe Track Recupero LibraryService per la gestione del file associato alla libreria Creato un Launcher tipo per testare la modale

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,10 @@ module emmepitre.com.emmepitre {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
-
+    requires com.fasterxml.jackson.databind;
+    requires atlantafx.base;
+    opens controllers to javafx.fxml;
+    opens models to com.fasterxml.jackson.databind;
     opens emmepitre.com.emmepitre to javafx.fxml;
     exports emmepitre.com.emmepitre;
 }
