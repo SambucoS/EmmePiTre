31dec3178b342c2a272409caefcd2943f6bd3e3a
ludmillatudino <ludmilla.tudino@gmail.com>
2026-06-01 16:27:27 +0200

    Creazione o modifica di Playlist.fxml, Playlist.java, PlaylistService.java, PlaylistController.java, module-info.java

---
+++ src/main/java/controllers/PlaylistController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import services.PlaylistService;
+
+public class PlaylistController {
+
+    @FXML
+    private TextField playlistNameField;
+
+    @FXML
+    private Label messageLabel;
+
+    private final PlaylistService playlistService = new PlaylistService();
+
+    @FXML
+    private void handleCreatePlaylist() {
+        String playlistName = playlistNameField.getText();
+
+        try {
+            playlistService.createPlaylist(playlistName);
+
+            messageLabel.setStyle("-fx-text-fill: green;");
+            messageLabel.setText("Playlist creata correttamente.");
+
+            playlistNameField.clear();
+
+        } catch (IllegalArgumentException e) {
+            messageLabel.setStyle("-fx-text-fill: red;");
+            messageLabel.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    private void handleCancel() {
+        playlistNameField.clear();
+        messageLabel.setText("");
+    }
+}
