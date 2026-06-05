eef6f849fc4d36d17602ca89c0025a3aadc69c43
Domenico <domenicoraimondo81@gmail.com>
2026-06-05 16:24:46 +0200

    Risolto in openPlayerView il caricamento erroneo di libraryView.fxml

--- src/main/java/controllers/LibraryViewController.java
+++ src/main/java/controllers/LibraryViewController.java
@@ -396,7 +396,7 @@ public class LibraryViewController implements LibraryObserver {
         try {
             // Creazione del caricatore FXML, specificando il path della view del Player
             FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/views/libraryView.fxml"));
+                    getClass().getResource("/views/playerView.fxml"));
 
 
             // Caricamento del contenuto del file fxml, l'albero dei nodi
